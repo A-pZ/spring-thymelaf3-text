caf4052c03df122f89730dbb2d8e827759a26798
A-pZ <a-pz@h4.dion.ne.jp>
2018-07-11 07:49:10 +0900

    Develop/spring boot2 (#3)
    * SpringBoot2.0.3 updated
    
    * remove duplicated jar.
    * use Thymeleaf-Spring5
    
    * change indent
    
    * delete unused config and properties.

--- src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
+++ src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
@@ -6,45 +6,41 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
-@PropertySource("classpath:mail/emailconfig.properties")
 public class ThymeleafConfig implements ApplicationContextAware, EnvironmentAware {
 	@Bean
-    public TemplateEngine textTemplateEngine() {
-        final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-        // Resolver for TEXT emails
-        templateEngine.addTemplateResolver(textTemplateResolver());
-        templateEngine.setTemplateEngineMessageSource(emailMessageSource());
-        return templateEngine;
-    }
+	public TemplateEngine textTemplateEngine() {
+		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+		templateEngine.addTemplateResolver(textTemplateResolver());
+		templateEngine.setTemplateEngineMessageSource(textMessageSource());
+		return templateEngine;
+	}
 
 	private ITemplateResolver textTemplateResolver() {
-        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
-        templateResolver.setOrder(Integer.valueOf(1));
-        //templateResolver.setResolvablePatterns(Collections.singleton("text/*"));
-        templateResolver.setPrefix("/mail/");
-        templateResolver.setSuffix(".txt");
-        templateResolver.setTemplateMode(TemplateMode.TEXT);
-        templateResolver.setCharacterEncoding("utf-8");
-        templateResolver.setCacheable(false);
-        return templateResolver;
-    }
+		final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setOrder(Integer.valueOf(1));
+		templateResolver.setPrefix("/mail/");
+		templateResolver.setSuffix(".txt");
+		templateResolver.setTemplateMode(TemplateMode.TEXT);
+		templateResolver.setCharacterEncoding("utf-8");
+		templateResolver.setCacheable(false);
+		return templateResolver;
+	}
 
-    @Bean
-    public ResourceBundleMessageSource emailMessageSource() {
-        final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("mail/MailMessages");
-        return messageSource;
-    }
+	@Bean
+	public ResourceBundleMessageSource textMessageSource() {
+		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("mail/MailMessages");
+		return messageSource;
+	}
 
 	@Override
 	public void setEnvironment(Environment environment) {
@@ -57,5 +53,5 @@ public class ThymeleafConfig implements ApplicationContextAware, EnvironmentAwar
 	}
 
 	private ApplicationContext applicationContext;
-    private Environment environment;
+	private Environment environment;
 }
