37c67ba0386583fcad788bc1389dce5efea46076
東 浩二 <ko_azuma@waku-2.com>
2018-07-10 10:11:11 +0900

    SpringBoot2.0.3 updated
    * remove duplicated jar.
    * use Thymeleaf-Spring5

--- src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
+++ src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.thymeleaf.TemplateEngine;
-import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.templatemode.TemplateMode;
 import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
