8cba65e9ebdc5bf0bc2472668af6745938d1c61a
A-pZ <g.alpha.pz@gmail.com>
2018-07-11 07:35:35 +0900

    delete unused config and properties.

--- src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
+++ src/main/java/com/github/apz/springsample/config/ThymeleafConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.core.env.Environment;
 import org.thymeleaf.TemplateEngine;
@@ -16,21 +15,18 @@ import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 import org.thymeleaf.templateresolver.ITemplateResolver;
 
 @Configuration
-@PropertySource("classpath:mail/emailconfig.properties")
 public class ThymeleafConfig implements ApplicationContextAware, EnvironmentAware {
 	@Bean
 	public TemplateEngine textTemplateEngine() {
 		final SpringTemplateEngine templateEngine = new SpringTemplateEngine();
-		// Resolver for TEXT emails
 		templateEngine.addTemplateResolver(textTemplateResolver());
-		templateEngine.setTemplateEngineMessageSource(emailMessageSource());
+		templateEngine.setTemplateEngineMessageSource(textMessageSource());
 		return templateEngine;
 	}
 
 	private ITemplateResolver textTemplateResolver() {
 		final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
 		templateResolver.setOrder(Integer.valueOf(1));
-		//templateResolver.setResolvablePatterns(Collections.singleton("text/*"));
 		templateResolver.setPrefix("/mail/");
 		templateResolver.setSuffix(".txt");
 		templateResolver.setTemplateMode(TemplateMode.TEXT);
@@ -40,7 +36,7 @@ public class ThymeleafConfig implements ApplicationContextAware, EnvironmentAwar
 	}
 
 	@Bean
-	public ResourceBundleMessageSource emailMessageSource() {
+	public ResourceBundleMessageSource textMessageSource() {
 		final ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 		messageSource.setBasename("mail/MailMessages");
 		return messageSource;
